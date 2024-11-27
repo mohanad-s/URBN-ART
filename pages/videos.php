739807d6f4158973c80ae000d0310e10d2797e63
mohanad-s <maaldakheel@stu.kau.edu.sa>
2024-11-27 04:35:50 +0300

    Changes to be committed: 	modified:   CSS/style.css 	new file:   pages/videos.php

---
+++ pages/videos.php
@@ -0,0 +1,78 @@
+<?php
+$pageTitle = 'Video Tutorials';
+include_once '../includes/header.php';
+?>
+
+<div class="video-container">
+    <h2>URBN ART Video Tutorials</h2>
+    
+    <div class="video-grid">
+        <div class="video-item">
+            <h3>How to Use URBN Gold Chrome</h3>
+            <div class="video-wrapper">
+                <object 
+                    data="https://www.youtube.com/embed/dQw4w9WgXcQ" 
+                    width="560" 
+                    height="315">
+                    <param name="movie" value="https://www.youtube.com/embed/dQw4w9WgXcQ">
+                    <param name="allowFullScreen" value="true">
+                    <param name="allowscriptaccess" value="always">
+                    <embed 
+                        src="https://www.youtube.com/embed/dQw4w9WgXcQ" 
+                        type="application/x-shockwave-flash" 
+                        allowscriptaccess="always" 
+                        allowfullscreen="true" 
+                        width="560" 
+                        height="315">
+                </object>
+            </div>
+            <p>Master the chrome effect with our premium spray paint - Step by step tutorial.</p>
+        </div>
+
+        <div class="video-item">
+            <h3>Graffiti Character Design</h3>
+            <div class="video-wrapper">
+                <object 
+                    data="https://www.youtube.com/embed/dQw4w9WgXcQ" 
+                    width="560" 
+                    height="315">
+                    <param name="movie" value="https://www.youtube.com/embed/dQw4w9WgXcQ">
+                    <param name="allowFullScreen" value="true">
+                    <param name="allowscriptaccess" value="always">
+                    <embed 
+                        src="https://www.youtube.com/embed/dQw4w9WgXcQ" 
+                        type="application/x-shockwave-flash" 
+                        allowscriptaccess="always" 
+                        allowfullscreen="true" 
+                        width="560" 
+                        height="315">
+                </object>
+            </div>
+            <p>Learn how to create stunning graffiti characters using URBN ART supplies.</p>
+        </div>
+
+        <div class="video-item">
+            <h3>Product Showcase: URBN Gold Series</h3>
+            <div class="video-wrapper">
+                <object 
+                    data="https://www.youtube.com/embed/dQw4w9WgXcQ" 
+                    width="560" 
+                    height="315">
+                    <param name="movie" value="https://www.youtube.com/embed/dQw4w9WgXcQ">
+                    <param name="allowFullScreen" value="true">
+                    <param name="allowscriptaccess" value="always">
+                    <embed 
+                        src="https://www.youtube.com/embed/dQw4w9WgXcQ" 
+                        type="application/x-shockwave-flash" 
+                        allowscriptaccess="always" 
+                        allowfullscreen="true" 
+                        width="560" 
+                        height="315">
+                </object>
+            </div>
+            <p>Discover our complete URBN Gold series and their unique features.</p>
+        </div>
+    </div>
+</div>
+
+<?php include_once '../includes/footer.php'; ?>
