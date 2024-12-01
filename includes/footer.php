d4f2940fe4fb2c111357a98869840c51fc570e40
mohanad-s <maaldakheel@stu.kau.edu.sa>
2024-12-01 17:43:21 +0300

    Changes to be committed: 	modified:   includes/footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -4,6 +4,22 @@
         <address>
             Contact us at: <a href="mailto:info@urbnart.com">info@urbnart.com</a>
         </address>
+        <p>
+        <a href="https://validator.w3.org/check?uri=referer" target="_blank">
+            <img 
+                src="https://www.w3.org/Icons/valid-xhtml10-blue" 
+                alt="Valid XHTML 1.0" 
+                style="border:0;width:88px;height:31px"
+            />
+        </a>
+        <a href="https://jigsaw.w3.org/css-validator/check/referer" target="_blank">
+            <img 
+                src="https://jigsaw.w3.org/css-validator/images/vcss-blue" 
+                alt="Valid CSS" 
+                style="border:0;width:88px;height:31px"
+            />
+        </a>
+    </p>
     </footer>
 </body>
 </html>
