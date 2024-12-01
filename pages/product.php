a15056ba44623e780211dee6d85bf51a3ae7a329
mohanad-s <maaldakheel@stu.kau.edu.sa>
2024-12-01 22:05:37 +0300

    Changes to be committed: 	modified:   pages/product.php

--- pages/product.php
+++ pages/product.php
@@ -46,11 +46,42 @@ if (!$product) {
         </div>
 
         <?php if ($product['stock_quantity'] > 0): ?>
-            <button onclick="addToCart(<?php echo $product['id']; ?>)">Add to Cart</button>
-        <?php else: ?>
-            <button disabled>Out of Stock</button>
-        <?php endif; ?>
+    <button type="button" onclick="addToCart(<?php echo $product['id']; ?>)">Add to Cart</button>
+<?php else: ?>
+    <button disabled>Out of Stock</button>
+<?php endif; ?>
     </div>
+
+    <script>
+function addToCart(productId) {
+    console.log('Adding product ID:', productId); // Debug line
+    
+    fetch('add_to_cart.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: 'product_id=' + productId
+    })
+    .then(response => {
+        console.log('Response:', response); // Debug line
+        return response.json();
+    })
+    .then(data => {
+        console.log('Data:', data); // Debug line
+        if (data.success) {
+            alert('Item added to cart successfully!');
+            location.reload();
+        } else {
+            alert(data.error || 'Error adding item to cart');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error); // Debug line
+        alert('Error adding item to cart');
+    });
+}
+</script>
 </section>
 
 <?php include_once '../includes/footer.php'; ?>
