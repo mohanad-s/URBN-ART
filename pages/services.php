cf17137e36e221963b29ea586bbfbfae750d9923
mohanad-s <maaldakheel@stu.kau.edu.sa>
2024-12-01 18:01:14 +0300

    Changes to be committed: 	modified:   pages/services.php

--- pages/services.php
+++ pages/services.php
@@ -1,23 +1,23 @@
-<?php
+<?php 
 $pageTitle = 'Services';
 include_once '../includes/header.php';
 ?>
 
-<main class="services-menu">
+<section class="services-menu">
     <h2>Our Services</h2>
 
     <div class="menu-buttons">
-    <button onclick="window.location.href='../includes/index.php'"><b>Home</b></button>
-    <button onclick="window.location.href='products.php'"><b>Products</b></button>
-    <button onclick="window.location.href='videos.php'"><b>Video Tutorials</b></button>
-    <button onclick="window.location.href='gallery.php'"><b>Gallery</b></button>
-    <button onclick="window.location.href='workshops.php'"><b>Workshops</b></button>
-    <button onclick="window.location.href='cart.php'"><b>Shopping Cart</b></button>
-    <button onclick="window.location.href='contact.php'"><b>Contact Us</b></button>
-    <button onclick="window.location.href='resume.php'"><b>About Owner</b></button>
-    <button onclick="window.location.href='feedback.php'"><b>Give Us Your Feedback!</b></button>
-</div>
-</main>
+        <button onclick="window.location.href='../includes/index.php'"><b>Home</b></button>
+        <button onclick="window.location.href='products.php'"><b>Products</b></button>
+        <button onclick="window.location.href='videos.php'"><b>Video Tutorials</b></button>
+        <button onclick="window.location.href='gallery.php'"><b>Gallery</b></button>
+        <button onclick="window.location.href='workshops.php'"><b>Workshops</b></button>
+        <button onclick="window.location.href='cart.php'"><b>Shopping Cart</b></button>
+        <button onclick="window.location.href='contact.php'"><b>Contact Us</b></button>
+        <button onclick="window.location.href='resume.php'"><b>About Owner</b></button>
+        <button onclick="window.location.href='feedback.php'"><b>Give Us Your Feedback!</b></button>
+    </div>
+</section>
 
 <?php
 include_once '../includes/footer.php';
